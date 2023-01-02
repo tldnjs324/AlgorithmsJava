adfe54bb144630124f61c30cd9f12e38fd9387e3
Siwon <73284904+tldnjs324@users.noreply.github.com>
2023-01-03 02:28:46 +0900

    d1

---
+++ AlgorithmsJava/src/Solution_2046_d1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution_2046_d1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+
+		for(int i = 0; i<a; i++) {
+			System.out.print("#");
+		}
+	}
+
+}
